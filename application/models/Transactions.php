d9d6e0d22cc5d2b53b858dedcbb4360779786eaf
nsol hub <44431757+nsolhub@users.noreply.github.com>
2019-03-01 15:27:40 +0500

    new sql queries

--- application/models/Transactions.php
+++ application/models/Transactions.php
@@ -12,9 +12,23 @@ class Transactions extends CI_Model
 	function fetch_transactions($id){
 	  $this->db->select('*');
 	  $this->db->from('transactions');
+<<<<<<< HEAD
+	  $this->db->where('user_id', $id);
+	  $this->db->order_by('created_at', 'dec');
+	  $query=$this->db->get();
+	  return $query->result();
+		// $this->db->where('user_id', $id);
+		// $query = $this->db->get('accounts');
+		// return $query->result();
+	}
+	function fetch_transactions1($id){
+	  $query = $this->db->query("select * from transactions where user_id = " . $id );
+	  return $query->result();
+=======
 	  $this->db->where('user_id',$id);
 	  $query=$this->db->get();
 	  return $query;
+>>>>>>> 6eabb6d25c2e598ca774cb784db26d63c72f3ef2
 		// $this->db->where('user_id', $id);
 		// $query = $this->db->get('accounts');
 		// return $query->result();
--- application/models/Transfers.php
+++ application/models/Transfers.php
@@ -9,6 +9,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Transfers extends CI_Model
 {
 
+<<<<<<< HEAD
+	function fetch_transfers($id){
+	  $this->db->select('*');
+	  $this->db->from('transfers');
+	  $this->db->where('user_id', $id);
+	  $this->db->order_by('created_at', 'dec');
+	  $query=$this->db->get();
+	  return $query->result();
+=======
 	function fetch_accounts($id){
 
 			      $this->db->select('*');
@@ -22,6 +31,7 @@ class Transfers extends CI_Model
 			      $row=$query->row();
 
 			      return $row;
+>>>>>>> 6eabb6d25c2e598ca774cb784db26d63c72f3ef2
 		// $this->db->where('user_id', $id);
 		// $query = $this->db->get('accounts');
 		// return $query->result();
